40c5501d7887ebc3cbf6845662b6d90ee50c7463
Dave Spina <dspina79@gmail.com>
2021-03-20 21:48:12 -0400

    Handling menu bar display and strikethrough

--- TextTransformer/TextTransformer/AppDelegate.swift
+++ TextTransformer/TextTransformer/AppDelegate.swift
@@ -9,18 +9,35 @@ import Cocoa
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
-
+    let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
     
 
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        statusItem.button?.title = "𝖆"
+        statusItem.button?.target = self
+        statusItem.button?.action = #selector(showSettings)
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
 
+    @objc func showSettings() {
+        let storyboard = NSStoryboard(name: "Main", bundle: nil)
+        guard let vc = storyboard.instantiateController(withIdentifier: "ViewController") as? ViewController else {
+            fatalError("Unable to find view controller in storyboard")
+        }
+        
+        guard let button = statusItem.button else {
+            fatalError("Could not find button")
+        }
+        
+        let popoverView = NSPopover()
+        popoverView.contentViewController = vc
+        popoverView.behavior = .transient
+        popoverView.show(relativeTo: button.bounds, of: button, preferredEdge: .maxY)
+    }
 
 }
 
--- TextTransformer/TextTransformer/ViewController.swift
+++ TextTransformer/TextTransformer/ViewController.swift
@@ -13,24 +13,18 @@ class ViewController: NSViewController, NSTextFieldDelegate {
     @IBOutlet weak var type: NSSegmentedCell!
     @IBOutlet weak var input: NSTextField!
     
-    func controlTextDidChange(_ obj: Notification) {
-        typeChanged(self)
-    }
     
-    func rot13(_ input: String) -> String {
-        return "ROT13: " + input
-    }
-    
-    func strikeThrough(_ input: String) -> String {
-        return "Strikethrough: " + input
-    }
-    
-    func similar(_ input: String) -> String {
-        return "Similar: " + input
-    }
-    
-    func zalgo(_ input: String) -> String {
-        return "Zalgo: " + input
+    @IBAction func typeChanged(_ sender: Any) {
+        switch type.selectedSegment {
+        case 0:
+            output.stringValue = rot13(input.stringValue)
+        case 1:
+            output.stringValue = similar(input.stringValue)
+        case 2:
+            output.stringValue = strikeThrough(input.stringValue)
+        default:
+            output.stringValue = zalgo(input.stringValue)
+        }
     }
     
     override func viewDidLoad() {
@@ -45,20 +39,34 @@ class ViewController: NSViewController, NSTextFieldDelegate {
         }
     }
 
-    @IBAction func copyToPastboard(_ sender: Any) {
+    func controlTextDidChange(_ obj: Notification) {
+        typeChanged(self)
     }
     
-    @IBAction func typeChanged(_ sender: Any) {
-        switch type.selectedSegment {
-        case 0:
-            output.stringValue = rot13(input.stringValue)
-        case 1:
-            output.stringValue = similar(input.stringValue)
-        case 2:
-            output.stringValue = strikeThrough(input.stringValue)
-        default:
-            output.stringValue = zalgo(input.stringValue)
-        }
+    func rot13(_ input: String) -> String {
+        return "ROT13: " + input
     }
+    
+    func strikeThrough(_ input: String) -> String {
+        var output = ""
+        
+        for letter in input {
+            output.append(letter)
+            output.append("\u{0335}")
+        }
+        return output
+    }
+    
+    func similar(_ input: String) -> String {
+        return "Similar: " + input
+    }
+    
+    func zalgo(_ input: String) -> String {
+        return "Zalgo: " + input
+    }
+
+    @IBAction func copyToPastboard(_ sender: Any) {
+    }
+
 }
 
